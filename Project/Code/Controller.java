05e16e1512df6797e4d790638f5e74e04fe768eb
dizhal <dmitriy.oven03@gmail.com>
2019-10-17 17:56:07 +0300

    Add 2 files

--- Project/Code/Controller.java
+++ Project/Code/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package Code;
 
 public class Controller {
 }
--- Project/Code/Main.java
+++ Project/Code/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package Code;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
